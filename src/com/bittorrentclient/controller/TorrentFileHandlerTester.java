9a587a730530e4502050e3af24d269a96e041746
sahilgupta5 <sahilgupta5@hotmail.com>
2016-03-28 23:12:29 -0400

    Adding the correct class doc for main bit torrent controller

--- src/com/bittorrentclient/controller/TorrentFileHandlerTester.java
+++ src/com/bittorrentclient/controller/TorrentFileHandlerTester.java
@@ -29,30 +29,6 @@ import com.bittorrentclient.services.ToolKit;
 import com.bittorrentclient.services.Utilities;
 import com.bittorrentclient.services.Utils;
 
-/*
- * Copyright 2006 Robert Sterling Moore II
- * 
- * This computer program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option) any
- * later version.
- * 
- * This computer program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this computer program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- */
-//package main;
-
-/**
- * A class used as an example of how to use the TorrentFileHandler class.
- * 
- * @author Robert S. Moore II
- */
 public class TorrentFileHandlerTester {
 	private TorrentFileHandler torrent_file_handler;
 
